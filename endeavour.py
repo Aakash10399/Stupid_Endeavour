641cc3c04c6123ce93b046ce7e8a19cd0db1a896
Aakash Pahwa <aakash.pahwa10399cric@gmail.com>
2016-12-05 13:09:06 +0530

    UPDATE_2

--- endeavour.py
+++ endeavour.py
@@ -3,13 +3,8 @@ import requests
 import re
 def printRequests(link):
     link1 = "https://www.google.co.in/search?q="
-    temp = link.split()
-    for word in temp:
-        link1 = link1 + word + "+"
-    link1 = link1[:len(link1)-2]    
-    r = requests.get(link1)
-    a = re.sub('<[^<]+?>', '', r.text)
-    b = "Spouse"
-    f = a[a.find(b):]
-    return f.count("(m. 1") + f.count("(m. 2")
+    for word in link.split():
+        link1 = link1 + word + "+"    
+    a = re.sub('<[^<]+?>', '', requests.get(link1[:len(link1)-2]).text)
+    return a[a.find("Spouse"):].count("(m. 1") + a[a.find("Spouse"):].count("(m. 2")
 print "No. of Spouses : "+str(printRequests(raw_input("Enter Celebrity Name : ")))
