9c6009da92fa33bcf33b6e39bb9df79a50486b0c
Aakash Pahwa <aakash.pahwa10399cric@gmail.com>
2016-12-07 13:14:33 +0530

    UPDATE_5

---
+++ endeavour_py3.py
@@ -0,0 +1,10 @@
+#ThankYouGoogle
+import requests
+import re
+def printRequests(link):
+    link1 = "https://www.google.co.in/search?q="
+    for word in link.split():
+        link1 = link1 + word + "+"    
+    a = re.sub('<[^<]+?>', '', requests.get(link1[:len(link1)-2]).text)
+    return a[a.find("Spouse"):].count("(m. 1") + a[a.find("Spouse"):].count("(m. 2")
+print("No. of Marriages : "+str(printRequests(input("Enter Celebrity Name : "))))
